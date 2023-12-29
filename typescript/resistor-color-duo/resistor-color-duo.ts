ee9e3c71821a1b03d4736d2cbd0f12c5c6db994c
Billy McCrackin <44419353+bmc1995@users.noreply.github.com>
2023-12-28 18:05:21 -0700

    complete resistor-color-duo

--- typescript/resistor-color-duo/resistor-color-duo.ts
+++ typescript/resistor-color-duo/resistor-color-duo.ts
@@ -1,3 +1,20 @@
-export function decodedValue() {
-  throw new Error('Remove this statement and implement this function')
+const COLORS = [
+  "black",
+  "brown",
+  "red",
+  "orange",
+  "yellow",
+  "green",
+  "blue",
+  "violet",
+  "grey",
+  "white",
+];
+const colorCode = (color: string) => {
+  return COLORS.indexOf(color);
+};
+export function decodedValue(colorDuoArr: string[]) {
+  const color1 = colorCode(colorDuoArr[0]);
+  const color2 = colorCode(colorDuoArr[1]);
+  return Number(`${color1}${color2}`);
 }
