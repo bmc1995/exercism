6a60ef7e8ec0d415a44b77dbc1edf94db208454d
Billy McCrackin <44419353+bmc1995@users.noreply.github.com>
2023-12-31 10:29:35 -0700

    complete rna-transcription

--- typescript/rna-transcription/rna-transcription.ts
+++ typescript/rna-transcription/rna-transcription.ts
@@ -1,3 +1,18 @@
-export function toRna() {
-  throw new Error('Remove this statement and implement this function')
+const complements: { [key: string]: string } = {
+  G: "C",
+  C: "G",
+  T: "A",
+  A: "U",
+};
+
+export function toRna(dnaSequence: string) {
+  return dnaSequence
+    .split("")
+    .map((n) => {
+      if (!complements[n]) {
+        throw new Error("Invalid input DNA.");
+      }
+      return complements[n];
+    })
+    .join("");
 }
