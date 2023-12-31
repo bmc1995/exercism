e18f0ddae6ea331f68169c6aa29631bd2b0ca9e7
Billy McCrackin <44419353+bmc1995@users.noreply.github.com>
2023-12-30 17:55:25 -0700

    ref(resistor-trio): cleanup

--- typescript/resistor-color-trio/resistor-color-trio.ts
+++ typescript/resistor-color-trio/resistor-color-trio.ts
@@ -12,31 +12,27 @@ const COLORS = [
 ];
 const prefixes = ["", "kilo", "mega", "giga"];
 
-const prefixer = (scaleFactor: number) => {
+const prefixer = (scaleFactor: number): string => {
   return prefixes[Math.floor(scaleFactor / 3)] + "ohms";
 };
 
-const colorCode = (color: string) => {
+const colorCode = (color: string): number => {
   return COLORS.indexOf(color);
 };
 
-const colorCodeValue = (colorStrArr: number[]) => {
+const colorCodeValue = (colorValArr: number[]): string => {
   const value = Number(
-    `${colorStrArr[0]}${colorStrArr[1]}${"0".repeat(colorStrArr[2])}`
+    `${colorValArr[0]}${colorValArr[1]}${"0".repeat(colorValArr[2])}`
   );
-  const scaleFactor = Math.floor(Math.log10(value));
+  let scaleFactor = value ? Math.floor(Math.log10(value)) : 0;
+
   const metricNum = value.toString().slice(0, (scaleFactor % 3) + 1);
 
   return `${metricNum} ${prefixer(scaleFactor)}`;
 };
 
-export function decodedResistorValue(colorTrioArr: string[]) {
-  const color1 = colorCode(colorTrioArr[0]);
-  const color2 = colorCode(colorTrioArr[1]);
-  const color3 = colorCode(colorTrioArr[2]);
-
-  if (!color1 && !color2) return "0 ohms";
-
-  const value = colorCodeValue([color1, color2, color3]);
+export function decodedResistorValue(colorTrioArr: string[]): string {
+  const colorCodes = colorTrioArr.map(colorCode);
+  const value = colorCodeValue(colorCodes);
   return value;
 }
