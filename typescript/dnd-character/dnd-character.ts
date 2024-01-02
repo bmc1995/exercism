8b80496d6ba43215ddd30e7a19ec99402d211b6f
Billy McCrackin <44419353+bmc1995@users.noreply.github.com>
2024-01-02 09:27:15 -0700

    begin pangram

--- typescript/dnd-character/dnd-character.ts
+++ typescript/dnd-character/dnd-character.ts
@@ -1,9 +1,12 @@
 export class DnDCharacter {
   public static generateAbilityScore(): number {
-    throw new Error('Remove this statement and implement this function')
+    throw new Error("Remove this statement and implement this function");
   }
 
   public static getModifierFor(abilityValue: number): number {
-    throw new Error('Remove this statement and implement this function')
+    // subtract 10, div 2, round down
+    return Math.floor((abilityValue - 10) / 2);
   }
+
+  public hitpoints: number = 0;
 }
