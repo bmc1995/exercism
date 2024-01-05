4bbdc47319f7d24ce4b87fdcc026fba92aacc87e
Billy McCrackin <44419353+bmc1995@users.noreply.github.com>
2024-01-05 14:59:54 -0700

    complete dnd-character

--- typescript/dnd-character/dnd-character.ts
+++ typescript/dnd-character/dnd-character.ts
@@ -1,12 +1,34 @@
 export class DnDCharacter {
+  strength: number;
+  dexterity: number;
+  constitution: number;
+  intelligence: number;
+  wisdom: number;
+  charisma: number;
+  hitpoints: number;
+
+  constructor() {
+    this.strength = DnDCharacter.generateAbilityScore();
+    this.dexterity = DnDCharacter.generateAbilityScore();
+    this.constitution = DnDCharacter.generateAbilityScore();
+    this.intelligence = DnDCharacter.generateAbilityScore();
+    this.wisdom = DnDCharacter.generateAbilityScore();
+    this.charisma = DnDCharacter.generateAbilityScore();
+    this.hitpoints = 10 + DnDCharacter.getModifierFor(this.constitution);
+  }
+
+  private static rollDice(): number[] {
+    return [0, 0, 0, 0].map(() => Math.floor(Math.random() * 6) + 1);
+  }
+
   public static generateAbilityScore(): number {
-    throw new Error("Remove this statement and implement this function");
+    return this.rollDice()
+      .sort()
+      .slice(1)
+      .reduce((prev, curr) => prev + curr);
   }
 
   public static getModifierFor(abilityValue: number): number {
-    // subtract 10, div 2, round down
     return Math.floor((abilityValue - 10) / 2);
   }
-
-  public hitpoints: number = 0;
 }
