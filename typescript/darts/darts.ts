47930eb761dbf3c89211cc65aa7823056d824c32
Billy McCrackin <44419353+bmc1995@users.noreply.github.com>
2024-01-05 17:33:10 -0700

    complete darts

--- typescript/darts/darts.ts
+++ typescript/darts/darts.ts
@@ -1,3 +1,18 @@
-export function score(x: unknown, y: unknown): unknown {
-  throw new Error('Remove this statement and implement this function')
+export function score(x: number, y: number): number {
+  const outerRadius = 10;
+  const midRadius = 5;
+  const innerRadius = 1;
+
+  const distance = Math.sqrt(x ** 2 + y ** 2);
+
+  switch (true) {
+    case distance >= 0 && distance <= innerRadius:
+      return 10;
+    case distance > innerRadius && distance <= midRadius:
+      return 5;
+    case distance > midRadius && distance <= outerRadius:
+      return 1;
+    default:
+      return 0;
+  }
 }
