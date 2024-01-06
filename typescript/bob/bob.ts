44e531c5c8aa7668245c36383efa557f237cb96d
Billy McCrackin <44419353+bmc1995@users.noreply.github.com>
2024-01-05 19:35:15 -0700

    complete bob

--- typescript/bob/bob.ts
+++ typescript/bob/bob.ts
@@ -1,3 +1,15 @@
-export function hey(message: unknown): unknown {
-  throw new Error('Remove this statement and implement this function')
+export function hey(message: string): string {
+  message = message.replace(/[\d|\s]/g, "");
+
+  const isQuestion: boolean = message.endsWith("?");
+  const onlyLetters = message.replace(/[^\w]/g, "");
+  const isYelledAt: boolean =
+    onlyLetters !== "" && onlyLetters === onlyLetters.toUpperCase();
+
+  if (!message) return "Fine. Be that way!";
+  if (isQuestion && !isYelledAt) return "Sure.";
+  if (isYelledAt && isQuestion) return "Calm down, I know what I'm doing!";
+  if (isYelledAt) return "Whoa, chill out!";
+
+  return "Whatever.";
 }
